2ecd154a9c39c1c4e8a1f7cb7f4c7220fc57d71c
oisteing <66906363+oisteing@users.noreply.github.com>
2025-04-07 11:31:22 +0200

    Update navi.py

--- navi.py
+++ navi.py
@@ -4,6 +4,7 @@ pages = {
     "Simuleringer": [
         st.Page("kuglekugler_online.py", title="Kule kuler"),
         st.Page("trekke.py", title="Trekke brikker"),
+        st.Page("hhhh.py", title="HHHH eller HHHT?")
     ],
     "Ressurser": [
         st.Page("halfmean.py", title="Half of the average"),
