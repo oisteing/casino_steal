73fdc2455057cb68c588b8f88567ea5538a41dc9
oisteing <66906363+oisteing@users.noreply.github.com>
2025-04-10 11:39:40 +0200

    Update hhhh.py

--- hhhh.py
+++ hhhh.py
@@ -4,9 +4,10 @@ from random import randint
 st.title("Simulering av myntkast")
 
 # Brukerinput
-antallkast = st.number_input("Kor mange kast skal vi ha?", min_value=1, value=5, step=1)
-antallrunder = st.number_input(f"...og kor mange simuleringar skal vi gjera av desse {antallkast} kasta?", min_value=1, value=5, step=1)
+antallkast = st.number_input("Kor mange gonger skal vi kaste mynten?", min_value=1, value=10, step=1)
+antallrunder = st.number_input(f"...og kor mange simuleringar skal vi gjera av desse {antallkast} kasta?", min_value=1, step=1)
 
+lengde=st.number_input("Kor langt mønster skal vi undersøke? ", max_value=antallkast, value=4)
 # Kjør simulering når brukeren trykker på knappen
 if st.button("Køyr simulering"):
     hhhh = 0
@@ -15,34 +16,42 @@ if st.button("Køyr simulering"):
     ingen = 0
 
     st.write("### Resultat frå kvar simulering:")
+    st.write("Vi ser etter sekvensene "+ lengde*" **:red[K]**" + " og " + (lengde-1)* " **:red[K]**"+" **:blue[M]**")
+    
     for k in range(antallrunder):
         resultat = ""
         for i in range(antallkast):
             penge = randint(1, 2)
-            resultat += " :red[H]" if penge == 1 else " :blue[T]"
+            resultat += " **:red[K]**" if penge == 1 else " **:blue[M]**"
 
         st.write(resultat)
-
+        
+        
         # Sjekk mønstre
-        inneholder_hhhh = " :red[H] :red[H] :red[H] :red[H]" in resultat
-        inneholder_hhht = " :red[H] :red[H] :red[H] :blue[T]" in resultat
+        #inneholder_hhhh = " :red[H] :red[H] :red[H] :red[H]" in resultat
+        #inneholder_hhht = " :red[H] :red[H] :red[H] :blue[T]" in resultat
+        
 
-        if inneholder_hhhh and not inneholder_hhht:
-            hhhh += 1
-            st.write("→ HHHH")
-        elif inneholder_hhht and not inneholder_hhhh:
-            hhht += 1
-            st.write("→ HHHT")
-        elif inneholder_hhhh and inneholder_hhht:
-            begge += 1
-            st.write("→ Begge mønstra")
-        else:
-            ingen += 1
-            st.write("→ Ingen av mønstra")
+        inneholder_hhhh = lengde* " **:red[K]**" in resultat
+        inneholder_hhht = (lengde-1)* " **:red[K]**" +" **:blue[M]**" in resultat
+
+        if antallrunder<50:
+            if inneholder_hhhh and not inneholder_hhht:
+                hhhh += 1
+                st.write("→ "+lengde*" **:red[K]**")
+            elif inneholder_hhht and not inneholder_hhhh:
+                hhht += 1
+                st.write("→ "+(lengde-1)*" **:red[K]**"+" **:blue[M]**")
+            elif inneholder_hhhh and inneholder_hhht:
+                begge += 1
+                st.write("→ Begge mønstra")
+            else:
+                ingen += 1
+                st.write("→ Ingen av mønstra")
 
     st.write("---")
     st.write("## Oppsummering:")
     st.write(f"Begge mønstra: {begge}")
-    st.write(f"Bare HHHH: {hhhh}")
-    st.write(f"Bare HHHT: {hhht}")
+    st.write(f"Berre "+lengde*" **:red[K]**"+f": {hhhh}")
+    st.write(f"Berre "+(lengde-1)*" **:red[K]**"+" **:blue[M]**"+f": {hhht}")
     st.write(f"Ingen av mønstra: {ingen}")
